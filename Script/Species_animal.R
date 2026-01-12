a8a75be80e78b384d9d139fde0da895671c89570
Louise PIEDNOËL <Louise@FDC70.local>
2026-01-12 10:44:18 +0100

    Ajout de commentaires sur chaque commande

--- Script/Species_animal.R
+++ Script/Species_animal.R
@@ -9,24 +9,29 @@ library(ggplot2)
 ########################################################
 
 # Ici tu peux changer ton "working directory"
-setwd("/Volumes/SHARED/Git_Projects/Regime_alimentaire_chamois")
+setwd("~/REGIME_ALIM_CHAM_GIT/Regime_alimentaire_chamois")
 
+#importer le jeu de données espèces animales
 species_animal <- import_list("BDD/ANTAGENE-F1016-FDC70-Regime_Chamois-12S/ANTAGENE-F1016-FDC70-Regime_Chamois-12S-seuil_100-100-100.xlsx")
 str(species_animal)
 
+
 species_an_occ <- species_animal$`Occurrences seuil`
 
+#nettoyage des donnees (suppr espaces, changements tirets etc)
 colnames(species_an_occ) <- species_an_occ[2,] %>%
   str_replace_all(" ", "") %>%
   str_replace_all("-", "_") %>%
   stri_trans_general("Latin-ASCII")
 
+#attribution des classes d'objet par colonne
 species_an_occ1 <- species_an_occ %>%
   slice(-c(1, 2)) %>%
   mutate(across(c(Latitude, Longitude, Departement,
                   14:28, 30, 32, 34), ~as.numeric(as.character(.)))) %>%
   mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30"))
 
+#Graph de répartition des échantillons par espèce d'ongules 
 ggplot(species_an_occ1) +
   geom_bar(aes(x=Taxon1))
 
